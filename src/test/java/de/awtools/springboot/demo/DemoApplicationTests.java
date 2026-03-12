1f9f6d1ebc0831f95123b71263a24a376dd1a44d
gluehloch <gluehloch@googlemail.com>
2026-03-12 19:04:28 +0100

    New demo project for a Spring-Boot application build with Gradle.

---
+++ src/test/java/de/awtools/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.awtools.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
